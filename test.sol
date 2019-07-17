baa1c9eb8124dfa7d321cfc741cf1fc7f6fd07ca
Carlos Hernandez <carlos.e.hernandez85@gmail.com>
2019-07-17 16:33:56 -0600

    Election - initial commit

--- test.sol
+++ test.sol
@@ -3,7 +3,13 @@ pragma solidity 0.5.1;
 contract test {
     
     address internal owner;
-    
+    event Hello{string _msg}
+
+    constructor() public {
+        owner == msg.sender;
+    }
+
+
 
 
 
